a9c89adc74ff1d19aeea4c00bea99df6b0cc0fab
Renan Rocha <renansouza2310@gmail.com>
2024-05-29 20:50:25 -0300

    Altera arquivo de interfaces

---
+++ src/utils/databaseInterfaces.ts
@@ -0,0 +1,11 @@
+export interface IServerVersion {
+  server_version: string;
+}
+
+export interface IDatabaseMaxConnections {
+  max_connections: string;
+}
+
+export interface IDatabaseOpennedConnections {
+  count: number
+}
