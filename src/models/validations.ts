d34df57e02fa114b2f0a24d4ad8b1f0f120c866a
Renan Rocha <renansouza2310@gmail.com>
2024-06-27 21:55:33 -0300

    Corrige a validação de data e adiciona linha em branco.

--- src/models/validations.ts
+++ src/models/validations.ts
@@ -38,8 +38,9 @@ function validateCPF(cpf: string): boolean {
 
 function validateDate(date: Date): boolean {
   const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
-
-  if (!dateRegex.test(date.toISOString())) {
+  const parsedDate = new Date(date).toISOString();
+  
+  if (!dateRegex.test(parsedDate)) {
     throw new Error("Invalid date");
   }
 
