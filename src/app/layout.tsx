a7117cf0bf012ee109e6455827a74f66d01892fa
Renan Rocha <renansouza2310@gmail.com>
2024-08-11 11:37:07 -0300

    Cria página home, cria componente de header com navbar, adiciona dependências e adiciona imagens do logo da aplicação

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "./components/header";
+import Container from "./components/container";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["cyrillic"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${montserrat.className} bg-slate-100`}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import Image from "next/image";
+
+const Home = () => {
   return (
-    <main>
-      <h1>Church Plus</h1>
-    </main>
+    <div className="flex flex-col justify-center items-center h-[calc(100vh-64px)]">
+      <h1 className="md:text-3xl text-2xl text-center">
+        Bem-vindo ao Church Plus
+      </h1>
+      <Image
+        src="/logo_transparent.png"
+        width={500}
+        height={300}
+        alt="logo Church plus"
+      />
+    </div>
   );
-}
+};
+
+export default Home;
